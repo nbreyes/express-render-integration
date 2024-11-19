f86d9876d489026fa2b41600023cb32801522591
Neil Benedict Reyes <reyesneil211@gmail.com>
2024-11-19 12:01:47 +0800

    Add product-service

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+
 const express = require('express');
+const productService = require('./product-service');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("Render Deployment Project");
-});
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/products', productService);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
