2c0fe917a8c87975838f576bf81a4e8d728cbf44
Neil Benedict Reyes <reyesneil211@gmail.com>
2024-11-19 12:41:49 +0800

    Add frontend integration

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 const express = require('express');
 const productService = require('./product-service');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/index.html'));
+    });
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +17,7 @@ app.use(express.json());
 // Routes
 app.use('/products', productService);
 
+app.use('/', router);
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
