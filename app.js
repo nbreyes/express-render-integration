bac9c99787f809b464af982c680f5ebd1826a08e
nbreyes <129636232+nbreyes@users.noreply.github.com>
2024-11-23 08:19:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const productService = require('./product-service');
 const path = require('path');
 const router = express.Router();
 
-router.get('/', function(req, res) { 
-    res.sendFile(path.join(__dirname + '/index.html'));
-    });
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 const app = express();
 const port = process.env.PORT || 3000;
